83243fcfc29e22e3262943eaeb766003ff714df1
csh <tmdgjs7495@naver.com>
2023-03-06 15:05:26 +0900

    HashSet equals, hashCode

--- CollectionsFramework/set/hashset/HashSetEx3.java
+++ CollectionsFramework/set/hashset/HashSetEx3.java
@@ -1,12 +1,17 @@
 package set.hashset;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class HashSetEx3 {
 
     public static void main(String[] args) {
         Set set = new HashSet();
+
+        List list = new ArrayList();
+        Collections.addAll(list, 2, 2, 2, 2, 2, 3);
+
+        set.addAll(list);
+        System.out.println(set.containsAll(list));
 
         set.add("123");
         set.add("123");
@@ -24,6 +29,20 @@ class Person {
     Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Person) {
+            Person p = (Person) obj;
+            return this.name.equals(p.name) && this.age == p.age;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 
     @Override
