c28a5fac6409ccde850ca44db2e10c58974b05fe
csh <tmdgjs7495@naver.com>
2023-07-03 23:42:59 +0900

    [feat] : 람다와 Predicate 로 코드 넘겨주기

---
+++ 모던 자바 인 액션/ch1/AppleFilterV1.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 두 메서드 내부에 중복되는 코드가 많다.
+ */
+public class AppleFilterV1 {
+
+    public static void main(String[] args) {
+
+        List<Apple> apples = Apple.getApples();
+
+        System.out.println(filterGreenApple(apples));
+        System.out.println(filterHeavyApple(apples));
+    }
+
+    // 녹색 사과를 선택해서 리스트를 반환
+    public static List<Apple> filterGreenApple(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for(Apple apple : inventory) {
+            if(Color.GREEN.equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    // 150 이상 사과 선택해서 리스트를 반환
+    public static List<Apple> filterHeavyApple(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for(Apple apple : inventory) {
+            if(apple.getWeight() > 150) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+}
