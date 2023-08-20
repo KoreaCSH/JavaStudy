3e35531856a14069bf0fff097503b934c22f1d76
csh <tmdgjs7495@naver.com>
2023-08-20 22:17:19 +0900

    [feat] : 메서드 참조 연습

--- 모던 자바 인 액션/ch3/ch3_functionalInterface/FunctionEx.java
+++ 모던 자바 인 액션/ch3/ch3_functionalInterface/FunctionEx.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 
 public class FunctionEx {
 
+    // 자바가 제공하는 함수형 인터페이스 Function
+    // Function 은 인자 T 객체를 받고 R 객체를 return 한다.
     public static void main(String[] args) {
 
         List<Integer> lengthOfString = map(
---
+++ 모던 자바 인 액션/ch3/ch3_methodReference/MethodReferenceEx1.java
@@ -0,0 +1,37 @@
+package ch3_methodReference;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+@FunctionalInterface
+interface StringToIntProcessor {
+
+    Integer process(String str);
+
+}
+
+public class MethodReferenceEx1 {
+
+    public static void main(String[] args) {
+
+        String str1 = "123";
+
+        Integer num1 = stringToInt(str1, Integer::parseInt);
+
+        System.out.println(num1);
+        System.out.println(num1 == 123);
+
+        String str2 = "12345";
+
+        Integer num2 = stringToInt(str2, Integer::parseInt);
+
+        System.out.println(num2);
+        System.out.println(num2 == 12345);
+    }
+
+    public static Integer stringToInt(String str, StringToIntProcessor p) {
+        return p.process(str);
+    }
+
+}
