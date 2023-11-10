a9040fc8a1f0229953a682b36a98c1187eaa6b01
csh <tmdgjs7495@naver.com>
2023-11-10 11:10:54 +0900

    [feat] : 병렬 연산에 더 특화된 메서드 예제

--- 모던 자바 인 액션/src/jmh/java/parallel/ParallelStreamBenchmark.java
+++ 모던 자바 인 액션/src/jmh/java/parallel/ParallelStreamBenchmark.java
@@ -3,6 +3,7 @@ package parallel;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 @State(Scope.Benchmark)
@@ -15,7 +16,7 @@ public class ParallelStreamBenchmark {
 
     // Score : 3.195
     @Benchmark
-    public Long iterativeSum() {
+    public long iterativeSum() {
         long result = 0;
         for(long i = 1L; i <= N; i++) {
             result += i;
@@ -25,7 +26,7 @@ public class ParallelStreamBenchmark {
 
     // Score : 73.409
     @Benchmark
-    public Long sequentialSum() {
+    public long sequentialSum() {
         return Stream.iterate(1L, i -> i + 1)
                 .limit(N)
                 .reduce(0L, Long::sum);
@@ -33,9 +34,34 @@ public class ParallelStreamBenchmark {
 
     // Score : 195.046
     @Benchmark
-    public Long parallelSum() {
+    public long parallelSum() {
         return Stream.iterate(1L, i -> i + 1)
                 .limit(N)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    // 스트림 및 병렬이 더 느린 이유는
+    // 1. 반복 결과로 박싱된 객체가 만들어지므로 숫자를 더하려면 언박싱을 해야 한다.
+    // 2. 반복 작업은 병렬로 수행할 수 있는 독립 단위로 나누기가 어렵다.
+    // 즉, 병렬 프로그래밍을 오용하면 오히려 전체 프로그램의 성능이 더 나빠질 수도 있다.
+
+
+    // 병렬에 더 특화된 메서드 사용
+    // 병렬 연산을 하기 위해서는 연산에 사용될 데이터들이 청크 단위로 나누어져야 한다.
+    // rangeClosed() 는 청크로 분할할 수 있는 숫자 범위 생산 + 기본형 long 을 직접 사용하므로 박싱과 언박싱 오버헤드가 사라진다는 장점이 있다.
+
+    // Score : 3.139
+    @Benchmark
+    public long rangedSum() {
+        return LongStream.rangeClosed(1, N)
+                .reduce(0L, Long::sum);
+    }
+
+    // Score : 0.635
+    @Benchmark
+    public long parallelRangedSum() {
+        return LongStream.rangeClosed(1, N)
                 .parallel()
                 .reduce(0L, Long::sum);
     }
