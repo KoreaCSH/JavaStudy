ce881929d809eddc554341256be7310ba3d1c55f
csh <tmdgjs7495@naver.com>
2023-11-12 22:28:47 +0900

    [feat] : Fork/Join 프레임워크 예제

--- 모던 자바 인 액션/src/jmh/java/parallel/ParallelStreamBenchmark.java
+++ 모던 자바 인 액션/src/jmh/java/parallel/ParallelStreamBenchmark.java
@@ -1,5 +1,6 @@
 package parallel;
 
+import fork_join.ForkJoinSumCalculator;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
@@ -65,6 +66,11 @@ public class ParallelStreamBenchmark {
                 .parallel()
                 .reduce(0L, Long::sum);
     }
+
+//    @Benchmark
+//    public long recursiveTaskSum() {
+//        return ForkJoinSumCalculator.forkJoinSum(N);
+//    }
 
     @TearDown(Level.Invocation) // 매 번 벤치마크를 실행한 다음에는 가비지 컬렉터 동작 시도
     public void tearDown() {
