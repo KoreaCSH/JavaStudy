ba079703a7341ac2140d316dff8dfce5727b9fd5
csh <tmdgjs7495@naver.com>
2024-10-31 00:34:49 +0900

    [ch9] : 람다 테스팅

---
+++ 모던 자바 인 액션/src/main/java/ch9/lambda_testing/Point.java
@@ -0,0 +1,84 @@
+package ch9.lambda_testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Point {
+
+    private final int x;
+    private final int y;
+
+    private final static Comparator<Point> compareByXAndThenY =
+            comparing(Point::getX).thenComparing(Point::getY);
+
+    public static List<Point> moveAllPointsRightBy(List<Point> points, int x) {
+        return points.stream()
+                .map(p -> new Point(p.getX() + x, p.getY()))
+                .collect(Collectors.toList());
+    }
+
+    private Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point moveRightBy(int x) {
+        return new Point(this.x + x, this.y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Test
+    public void testMoveRightBy() throws Exception {
+        Point p1 = new Point(5, 5);
+        Point p2 = p1.moveRightBy(10);
+        assertEquals(15, p2.getX());
+        assertEquals(5, p2.getY());
+    }
+
+    @Test
+    public void testComparingTwoPoints() throws Exception {
+        Point p1 = new Point(10, 15);
+        Point p2 = new Point(10, 20);
+        int result = Point.compareByXAndThenY.compare(p1, p2);
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void testMoveAllPointsRightBy() throws Exception {
+        List<Point> points =
+                Arrays.asList(new Point(5, 5), new Point(10, 5));
+        List<Point> expectedPoints =
+                Arrays.asList(new Point(15, 5), new Point(20, 5));
+        List<Point> newPoints = Point.moveAllPointsRightBy(points, 10);
+        assertEquals(expectedPoints, newPoints);
+    }
+
+}
